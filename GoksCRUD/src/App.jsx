2d6cfe2b0dc80e8c83a73dcd8a49c8b1c1d38f96
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2024-02-14 03:14:53 +0800

    Basic Log In and Sign Up

---
+++ GoksCRUD/src/App.jsx
@@ -0,0 +1,15 @@
+import LogIn from "./Forms/LogIn.jsx";
+import SignUp from "./Forms/SignUp.jsx";
+
+function App() {
+  return (
+    <>
+      <div className="bg-green-900">
+        <LogIn></LogIn>
+        {/* <SignUp></SignUp> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ GoksCRUD/src/Forms/SignUp.jsx
@@ -0,0 +1,54 @@
+const SignUp = () => {
+  return (
+    <>
+      <div className="h-screen flex flex-col  items-center justify-center">
+        <div className="p-2 border-1 rounded-lg bg-green-500	shadow-lg shadow-black mb-20">
+          <div className="flex justify-center mt-4 mb-2 pb-2 font-bold text-3xl text-white">
+            <h1>Goks App:</h1>
+          </div>
+          <div className="mb-4 flex justify-center font-bold text-lg text-white">
+            Sign Up
+          </div>
+          <form action="" className="flex flex-col m-4 text-white">
+            <div className="m-3">
+              <label htmlFor="userName">
+                Username:{" "}
+                <input
+                  className="border-1 bg-green-600 rounded-lg p-2 ml-2 hover:bg-green-700 transition duration-300 ease-in-out"
+                  type="text"
+                  name="userName"
+                />
+              </label>
+            </div>
+            <div className="m-3">
+              <label htmlFor="userName">
+                Password:{" "}
+                <input
+                  className="border-1 bg-green-600 rounded-lg p-2 ml-2 hover:bg-green-700 transition duration-300 ease-in-out"
+                  type="password"
+                  name="userName"
+                />
+              </label>
+            </div>
+            <div className="flex justify-center m-3">
+              <input
+                className="border-1 font-bold bg-green-600	rounded-lg px-4 py-2 hover:bg-green-700 transition duration-300 ease-in-out"
+                type="button"
+                value="Log In"
+              />
+            </div>
+            <div className="flex justify-center m-3">
+              <input
+                className="border-1 font-bold bg-green-600 rounded-lg px-4 py-2 text-xs hover:bg-green-700 transition duration-300 ease-in-out"
+                type="button"
+                value="Create Account"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
